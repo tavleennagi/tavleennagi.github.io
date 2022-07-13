79090f6e430c8036b62ea69437a53fb03fdc2a19
tavleennagi <68106770+tavleennagi@users.noreply.github.com>
2022-07-13 20:43:50 +0530

    Update script.js

--- Nagarro_Training/FormValidation/script.js
+++ Nagarro_Training/FormValidation/script.js
@@ -1,94 +1,111 @@
 $(document).ready(function(){
     $('#usernamevalidation').hide();
+    $('#emailvalidation').hide();
     $('#passwordvalidation').hide();
     $('#confirmpasswordvalidation').hide();
 
-    var Error=true;
-    var password_error=true;
-    var confirm_password_error=true;
+    var username_Error=true;
+    var password_Error=true;
+    var confirm_password_Error=true;
+    let email_Error = true;
+
 
     $('#username').keyup(function(){
         usernamevalidation();
     });
     function usernamevalidation(){
-        var username_val=$('#username').val();
-        if(username_val.length==""){
-            $('#usernamevalidation').show();
-            $('#usernamevalidation').html("Username cannot be empty");
-            $('#usernamevalidation').css('color','red');
-            Error=false;
-            return false;
-        }
-        else{
-            $('#usernamevalidation').hide();
-        }
-
-        if(username_val.length<3 | username_val.length>10){
-            $('#usernamevalidation').show();
-            $('#usernamevalidation').html("Invalid Username");
-            $('#usernamevalidation').css('color','red');
-            Error=false;
-            return false;
-        }
-        else{
-            $('#usernamevalidation').hide();
-        }
-
-    }
+        var usernameValue=$('#username').val();
+        const usernameRegex = new RegExp(/^[a-z0-9_-]{3,10}$/gim);
+        if (usernameValue.length === 0) {
+            $("#usernamevalidation").show();
+            $("#usernamevalidation").text("Username can't be empty");
+            username_Error = true;
+          } else if (!usernameRegex.test(usernameValue)) {
+            $("#usernamevalidation").show();
+            $("#usernamevalidation").text("Invalid Username");
+            username_Error = true;
+          } else {
+            $("#usernamevalidation").hide();
+            username_Error = false;
+          }
+        };
+     
+    $("#email").keyup(() => {
+            emailValidation();
+        });
+        
+    const emailValidation = () => {
+      let emailValue = $("#email").val();
+            const emailRegex = new RegExp(
+              /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            );
+        if (emailValue.length === 0) {
+              $("#emailvalidation").show();
+              $("#emailvalidation").text("Email id can't be empty");
+              email_Error = true;
+         } else if (!emailRegex.test(emailValue)) {
+              $("#emailvalidation").show();
+              $("#emailvalidation").text("Invalid Email id");
+              email_Error = true;
+        } else {
+              $("#emailvalidation").hide();
+              email_Error = false;
+            }
+          };
 
     $('#password').keyup(function(){
         passwordvalidation();
     });
     function passwordvalidation(){
-        var password_val=$('#password').val();
-        if(password_val.length==""){
-            $('#passwordvalidation').show();
-            $('#passwordvalidation').html("Password cannot be empty");
-            $('#passwordvalidation').css('color','red');
-            password_error=false;
-            return false;
-        }
-        else{
-            $('#passwordevalidation').hide();
-        }
+        var passwordValue=$('#password').val();
+        const strongRegex = new RegExp(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+          );
+          if (passwordValue.length === 0) {
+            $("#passwordvalidation").show();
+            $("#passwordvalidation").text("Password can't be empty");
+            password_Error = true;
+          } else if (!strongRegex.test(passwordval)) {
+            $("#passwordvalidation").show();
+            $("#passwordvalidation").text("Invalid Password");
+            password_Error = true;
+          } else {
+            $("#passwordvalidation").hide();
+            password_Error = false;
+          }
+        };
 
-        if(password_val.length<8 ){
-            $('#passwordvalidation').show();
-            $('#passwordvalidation').html("Password must be greater than 8");
-            $('#passwordvalidation').css('color','red');
-            password_error=false;
-            return false;
-        }
-        else{
-            $('usernamevalidation').hide();
-        }
-    }
-    $('#confirmpassword').keyup(function(){
-        confirm_password();
-    });
-    function confirm_password(){
-        var confirm_password_val=$('#confirmpassword').val();
-        var password_val = $('#password').val();
-        if(password_val != confirm_password_val)
-        {
-            $('#confirmpasswordvalidation').show();
-            $('#confirmpasswordvalidation').html("Password did nto match");
-            $('#confirmpasswordvalidation').css('color','red');
-            password_error=false;
-            return false;
-        }
-        else{
-            $('confirmusernamevalidation').hide();
-        }
         
-    }
-    $('#submitvalidation').click(function(){
-
-        usernamevalidation();
-        passwordvalidation();
-        confirm_password()
+    $("#confirmpassword").keyup(() => {
+      confirmPasswordValidation();
     });
-
+      const confirmPasswordValidation = () => {
+      let confirmPasswordValue = $("#confirmpassword").val();
+      let passwordValue = $("#password").val();
+      if (confirmPasswordValue.length === 0) {
+            $("#confirmpasswordvalidation").show();
+            $("#confirmpasswordvalidation").text("Confirm Password can't be empty");
+            confirm_password_Error = true;
+       } else if (passwordValue !== confirmPasswordValue) {
+            $("#confirmpasswordvalidation").show();
+            $("#confirmpasswordvalidation").text(
+              "Password and Confirm Password needs to be same"
+            );
+            confirm_password_Error = true;
+       } else {
+            $("#confirmpasswordvalidation").hide();
+            confirm_password_Error = false;
+          }
+        };
+   $("#submit-btn").click(() => {
+      usernameValidation();
+      emailValidation();
+      passwordValidation();
+      confirmPasswordValidation();
+      if (username_Error || email_Error || password_Error || confirm_Password_Error) {
+        return false;
+      }
+    });
 
 
 });

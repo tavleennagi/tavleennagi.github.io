4a784064255095060725141d59f9cb411615fdfe
tavleennagi <68106770+tavleennagi@users.noreply.github.com>
2022-07-28 22:11:05 +0530

    Add files via upload

---
+++ Nagarro_Training/ThemeChanger/script.js
@@ -0,0 +1,24 @@
+function red(){
+    document.body.style.backgroundColor = 'red'
+    document.getElementById('written').style.backgroundColor='red'
+    document.getElementById('written').style.outline='3px solid white';
+    document.getElementById('written').style.color='white'
+}
+function black(){
+    document.body.style.backgroundColor = 'black'
+    document.getElementById('written').style.backgroundColor='black'
+    document.getElementById('written').style.outline='3px solid white';
+    document.getElementById('written').style.color='white'
+}
+function green(){
+    document.body.style.backgroundColor = 'green'
+    document.getElementById('written').style.backgroundColor='green'
+    document.getElementById('written').style.outline='3px solid black';
+    document.getElementById('written').style.color='black'
+}
+function yellow(){
+    document.body.style.backgroundColor = 'yellow'
+    document.getElementById('written').style.backgroundColor='yellow'
+    document.getElementById('written').style.outline='3px solid black';
+    document.getElementById('written').style.color='black'
+}
